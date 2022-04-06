8d2e6bb103be783bb3c84005c97bafe579f37e49
apitzl <89361121+apitzl@users.noreply.github.com>
2022-04-06 16:48:26 -0400

    Add files via upload
    eW^ZSrdxutfcyigvbohipjno[kml;

---
+++ milestone4_a.m
@@ -0,0 +1,9 @@
+%   Allison Pitzl
+%   Milestone 4 part A
+%   created 3/30/2022
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Fs = 48000;     % sample rate
+
+[y, Fs] = audioread('IQ.wav'); % reads IQ data from file
+
