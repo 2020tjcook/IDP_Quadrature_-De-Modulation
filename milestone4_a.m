c1ee9cb36faba156556090ad5447ce554ed426b1
Jimmy Cook <42844800+2020tjcook@users.noreply.github.com>
2022-04-06 17:19:30 -0400

    added calculation for IQ data

--- milestone4_a.m
+++ milestone4_a.m
@@ -3,7 +3,8 @@
 %   created 3/30/2022
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Fs = 48000;     % sample rate
-
 [y, Fs] = audioread('IQ.wav'); % reads IQ data from file
-
+for k = 1:length(y)
+    IQ(k) = y(k, 1) + i*y(k, 2);
+end
+[Pxx, W] = pwelch[IQ]
