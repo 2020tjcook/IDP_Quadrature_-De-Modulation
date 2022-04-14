6815299e7659e472c6ac2575ad6419fda8900e71
Jimmy Cook <42844800+2020tjcook@users.noreply.github.com>
2022-04-14 15:51:59 -0400

    Week 13 class updates

--- milestone4_a.m
+++ milestone4_a.m
@@ -11,5 +11,26 @@ semilogy(W, Pxx);
 [B, a] = butter(5, 40000/(Fs/2));
 IQfilt = filtfilt(B, a, IQ);
 [Pxx, W] = pwelch(IQfilt);
+figure(1)
 semilogy(W, Pxx); 
+figure(2)
 plot(IQfilt);
+
+for k = 2:length(IQfilt)
+    IQangle(k-1) = angle(IQfilt(k))-angle(IQfilt(k-1));
+end
+counter = 29+125;
+index = 1;
+while counter<length(IQangle)
+    if IQangle(counter) > 0
+        data(index) = 1;
+    else
+        data(index) = 0;
+    end
+    index = index + 1;
+    counter = counter+250;
+end
+
+for k= 1:2000
+    b(k) = IQangle(k);
+end
